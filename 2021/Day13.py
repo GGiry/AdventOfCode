162b72cceb1c60c1fa5773c3ab8b4f3a4f99970b
Geoffrey GIRY <giry.geoffrey@gmail.com>
2021-12-25 10:43:55 +0100

    Complete Day 13

--- 2021/Day13.py
+++ 2021/Day13.py
@@ -27,20 +27,48 @@ class Origami:
         for coordinate in coordinates:
             self.sheet[coordinate[1]][coordinate[0]] = "#"
 
+    def __repr__(self):
+        result = ""
+        for line in self.sheet:
+            for char in line:
+                result += char
+            result += "\n"
+        return result
+
     def fold(self):
         if len(self.folds) > 0:
             fold = self.folds.pop(0)
 
             if fold[0] == 'y':
-                max_x = len(self.sheet[0]) + 1
-                max_y = int((len(self.sheet) + 1) / 2)
+                max_x = len(self.sheet[0])
+                max_y = int(len(self.sheet) / 2)
                 sheet = [["." for _ in range(max_x)] for _ in range(max_y)]
-                for i in range(max_y):
-                    sheet[i] = self.sheet[i]
 
                 for i in range(max_y):
                     for j in range(max_x):
-                        sheet[i][j] = self.sheet[i][j], self.sheet[i][j + value]
+                        sheet[i][j] = self.combine(self.sheet[i][j], self.sheet[2 * max_y - i][j])
+                self.sheet = sheet
+            else:
+                max_x = len(self.sheet[0]) // 2
+                max_y = len(self.sheet)
+                sheet = [["." for _ in range(max_x)] for _ in range(max_y)]
+
+                for i in range(max_y):
+                    for j in range(max_x):
+                        sheet[i][j] = self.combine(self.sheet[i][j], self.sheet[i][2 * max_x - j])
+                self.sheet = sheet
+
+    @staticmethod
+    def combine(value1, value2):
+        if value1 == value2:
+            return value1
+        return "#"
+
+    def count_dots(self):
+        result = 0
+        for line in self.sheet:
+            result += line.count("#")
+        return result
 
 
 class Day13(unittest.TestCase):
@@ -79,3 +107,26 @@ class Day13(unittest.TestCase):
                           ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.'],
                           ['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']],
                          origami.sheet)
+
+    def test_example_1(self):
+        origami = Origami(read_input_str("input_day_13_example.txt"))
+        origami.fold()
+        self.assertEqual(17, origami.count_dots())
+
+    def test_input_1(self):
+        origami = Origami(read_input_str("input_day_13.txt"))
+        origami.fold()
+        self.assertEqual(678, origami.count_dots())
+
+    def test_example_2(self):
+        origami = Origami(read_input_str("input_day_13_example.txt"))
+
+        while len(origami.folds) != 0:
+            origami.fold()
+        print(origami)
+
+    def test_input_2(self):
+        origami = Origami(read_input_str("input_day_13.txt"))
+        while len(origami.folds) != 0:
+            origami.fold()
+        print(origami)
